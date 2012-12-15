4a12cafc459ec873483021f71886901aa5266c50
EliasZ <mail@eliaszerrouq.nl>
2012-12-15 17:41:25 +0100

    Folder contents, minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,68 @@
 $(function() {
+    var folder = $('#main .folder'),
+        front = folder.find('.front'),
+        contents_container = $('.folder-contents'),
+        img = $('#main img'),
+        droppedCount = 0,
+        isDragging = false,
+        hoveringOnContentsContainer = false;
 
-	var folder = $("#main .folder"),
-		front = folder.find('.front'),
-		img = $("#main img"),
-		droppedCount = 0;
+    img.draggable({
+        revert: 'invalid'
+    });
 
-	img.draggable();
-
-	folder.droppable({
-		drop : function(event, ui) {
-			
-			// Remove the dragged icon
-			ui.draggable.remove();
-			
-			// update the counters
-			front.text(++droppedCount);
-			
-		},
-		
-		activate : function(){
-			// When the user starts draggin an icon
-			folder.addClass('open');
-		},
-		
-		deactivate : function(){
-			// Close the folder
-			folder.removeClass('open');
-		}
-	});
+    folder.droppable({
+        accept: '#main img',
+        
+        drop: function(event, ui) {
+            // Move the dragged icon to .folder-contents
+            ui.draggable.appendTo(contents_container)
+                        .draggable('disable')
+                        .removeAttr('style')
+                        .css('opacity', 1);
+            
+            // update the counter
+            front.text(++droppedCount);
+        },
+        
+        activate: function(event, ui) {
+            isDragging = true;
+            folder.addClass('open');
+        },
+        
+        deactivate: function() {
+            isDragging = false;
+            folder.removeClass('open');
+        }
+    }).hover(
+        // handlerIn
+        function() {
+            folder.addClass('open');
+            
+            if (!isDragging)
+                contents_container.fadeIn();
+        }, 
+        
+        // handlerOut
+        function() {
+            folder.removeClass('open');
+            
+            if (!isDragging && !hoveringOnContentsContainer)
+                contents_container.fadeOut();
+        }
+    );
+    
+    contents_container.hover(
+        function()
+        {
+            hoveringOnContentsContainer = true;
+            contents_container.stop().fadeIn();
+        },
+        
+        function()
+        {
+            hoveringOnContentsContainer = false;
+            contents_container.fadeOut();
+        }
+    );
 });
